fd1f96dc082be0870b0d148725a45b4a147d5d1e
saburali <saburali.me@gmail.com>
2023-09-20 11:50:33 +0600

    Similar to the project you provided, I tried to create this new project.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Link, Route, Routes, Switch} from 'react-router-dom';
+
+import Index from "../src/components/Index";
+import Menu from "../src/components/Menu.js";
+import Problem1 from "../src/components/Problem-1";
+import Problem2 from "../src/components/Problem-2";
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/" element={<Menu />}>
+                <Route path="problem-1" element={<Problem1 />} />
+                <Route path="problem-2" element={<Problem2 />} />
+              </Route>
+            </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
